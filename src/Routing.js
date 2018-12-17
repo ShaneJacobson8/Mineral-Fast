a5465b9ba0888896ae7e63a6dc1b21bdada4a45e
Shane Jacobson <shane-jacobson@hotmail.com>
2018-12-16 22:09:46 -0700

    add firebase

---
+++ src/Routing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+            <p>Home</p>
+            <ul>
+            {/* <li><Link to= '/Form'>Form</Link></li>
+            <li><Link to= '/People'>People</Link></li> */}
+            </ul>
+            </div>
+        )
+    }
+}
+export default Home;
