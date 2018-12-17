e4b340b48c2550ebf0b78f3ad5ed2fef44a945ae
Shane Jacobson <shane-jacobson@hotmail.com>
2018-12-17 14:55:15 -0700

    Add Navbar

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ import * as serviceWorker from './serviceWorker';
 // }; 
 // firebase.initializeApp(config);
 
-// ReactDOM.render(
-//     <App />,
-//      document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
