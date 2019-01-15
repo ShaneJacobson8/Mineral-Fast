348292f0e6407ab57ea92bda73c3f9c4a8ceb1f3
Shane Jacobson <shane-jacobson@hotmail.com>
2019-01-15 15:24:13 -0700

    Revert "Revert "worked on admin""
    This reverts commit 6ed58473dd68e6ff2e88b8d2996008b9d2f8db02.

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import firebase, { auth, provider } from "../firebase.js";
 import "./Admin.css";
 
@@ -11,11 +11,16 @@ class Admin extends Component {
       signUpFirstName: "",
       signUpLastName: "",
       signUpEmailAddress: "",
-      user: null
-      // AdminFirstName: '',
-      // AdminLastName: '',
-      // AdminEmailAddress: '',
+      transformationFirstName: "",
+      transformationLastName: "",
+      transformationEmailAddress: "",
+      transformationMessage: "",
+      transformationFile: "",
+      user: null,
+      items: []
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
   }
@@ -24,14 +29,42 @@ class Admin extends Component {
       [e.target.name]: e.target.value
     });
   }
-  // TODO adjust displayed properties to match desired values***
   handleSubmit(e) {
     e.preventDefault();
     const itemsRef = firebase.database().ref("SignUp");
     const item = {
-      title: this.state.emailAddress,
-      user: this.state.user.displayName || this.state.user.email
+      // title: this.state.emailAddress,
+      // user: this.state.firstName || this.state.lastName,
+      firstName: this.state.signUpFirstName,
+      lastName: this.state.signUpLastName,
+      emailAddress: this.state.signUpEmailAddress,
+      // user: this.state.user.firstName || this.state.user.emailAddress
+    };
+    itemsRef.push(item);
+    this.setState({
+      signUpFirstName: "",
+      signUpLastName: "",
+      signUpEmailAddress: "",
+  });
     }
+    handleSubmit2(e) {
+      e.preventDefault();
+    const itemsRef = firebase.database().ref("Transformation");
+    const item = {
+      firstName: this.state.transformationFirstName,
+      lastName: this.state.transformationLastName,
+      emailAddress: this.state.transformationEmailAddress,
+      message: this.state.transformationMessage,
+      file: this.state.transformationFile
+    };
+    itemsRef.push(item);
+    this.setState({
+      transformationFirstName: "",
+      transformationLastName: "",
+      transformationEmailAddress: "",
+      transformationMessage: "",
+      transformationFile: ""
+    });
   }
   login() {
     auth.signInWithPopup(provider).then(result => {
@@ -54,95 +87,190 @@ class Admin extends Component {
         this.setState({ user });
       }
     });
+
+// Tracking SignUp items
+    const itemsRef = firebase.database().ref('SignUp');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user,
+          signUpFirstName: items[item].signUpFirstName,
+          signUpLastName: items[item].signUpLastName,
+          signUpEmailAddress: items[item].signUpEmailAddress
+        })
+      }
+      this.setState({
+        items: newState
+      })
+    })
+ 
   }
+  removeItem(itemId) {
+    const itemRef = firebase.database().ref(`/SignUp/${itemId}`);
+    itemRef.remove();
+  }
+
 
   render() {
     return (
       <div className="wrapper">
-        <br />
+        <br/>
         <header>
           <h3>Admin Sign In</h3>
-          <br />
+          <br/>
           {this.state.user ? (
             <button
               type="submit"
               className="btn btn-secondary"
-              onClick={this.logout}
-            >
+              onClick={this.logout}>
               Log Out
             </button>
           ) : (
             <button
               type="submit"
               className="btn btn-primary"
-              onClick={this.login}
-            >
+              onClick={this.login}>
               Log In
             </button>
           )}
+          
         </header>
-        {this.state.user ?
-      <div>
-        <div className='user-profile'>
-          <img src={this.state.user.photoURL} />
-        </div>
-        <div className='container'>
-        <section className='add-item'>
-        <form onSubmit={this.handleSubmit}>
-        <input type="text" name="displayName" placeholder="What's your name?" value={this.state.user.displayName || this.state.user.email} />
-        <input type="text" name="emailAddress" placeholder="What's your email address?" onChange={this.handleChange} value={this.state.emailAddress} /> 
-        <button type="submit" className="btn btn-primary">Add Subscriber</button>
-        </form>
-        </section>
-        </div>
+        {this.state.user ? (
+          <div>
+            <div className="user-profile">
+              <img src={this.state.user.photoURL} />
+            </div>
+            {/* add new subscriber */}
+            <Fragment>
+            <div className="subscriber-container">
+              <section className="add-subscriber">
+                <form onSubmit={this.handleSubmit}>
+                <h3>Add new subscriber</h3>
+                  {/* <input type="text" name="displayName" placeholder="What's your name?" value={this.state.user.displayName || this.state.user.email} /> */}
+                  <input
+                    type="text"
+                    name="signUpFirstName"
+                    className="form-control"
+                    placeholder="New Subscriber First Name"
+                    onChange={this.handleChange}
+                    value={this.state.signUpFirstName}
+                  />
+                  <input
+                    type="text"
+                    name="signUpLastName"
+                    className="form-control"
+                    placeholder="New Subscriber Last Name"
+                    onChange={this.handleChange}
+                    value={this.state.signUpLastName}
+                  />
+                  <input
+                    type="text"
+                    name="signUpEmailAddress"
+                    className="form-control"
+                    placeholder="New Subscriber email"
+                    onChange={this.handleChange}
+                    value={this.state.signUpEmailAddress}
+                  />
+                  <button type="submit" className="btn btn-primary">
+                    Add Subscriber
+                  </button>
+                </form>
+              </section>
+            </div>
+              {/* add new transformation */}
+              <div className="transformation-container">
+              <section className="add-transformation">
+                <form onSubmit={e => this.handleSubmit2(e)}>
+                {/* <form onSubmit={this.handleSubmit2}></form> */}
+                <h3>Add new transformation</h3>
+                  {/* <input type="text" name="displayName" placeholder="What's your name?" value={this.state.user.displayName || this.state.user.email} /> */}
+                  <input
+                    type="text"
+                    name="transformationFirstName"
+                    className="form-control"
+                    placeholder="New Transformation First Name"
+                    onChange={this.handleChange}
+                    value={this.state.transformationFirstName}
+                  />
+                  <input
+                    type="text"
+                    name="transformationLastName"
+                    className="form-control"
+                    placeholder="New Transformation Last Name"
+                    onChange={this.handleChange}
+                    value={this.state.transformationLastName}
+                  />
+                  <input
+                    type="text"
+                    name="transformationEmailAddress"
+                    className="form-control"
+                    placeholder="New Transformation email"
+                    onChange={this.handleChange}
+                    value={this.state.transformationEmailAddress}
+                  />
+                  <textarea
+                    name="transformationMessage"
+                    className="form-control"
+                    placeholder="Enter Tranformation Message"
+                    id="exampleFormControlTextarea1"
+                    rows="6"
+                    onChange={this.handleChange}
+                    value={this.state.transformationMessage}
+                  />
+                  <input
+                    name="transformationFile"
+                    type="file"
+                    className="form-control-file"
+                    id="exampleFormControlFile1"
+                    onChange={this.handleChange}
+                    value={this.state.transformationFile}
+                  />
+                  <button type="submit2" className="btn btn-primary">
+                    Add Transformation
+                  </button>
+                </form>
+              </section>
+            </div>
+            </Fragment>
+            {/* return SignUp List */}
+  <div className='display-container'>
 
-          {/* <section className='display-item'>
-          <div className="wrapper">
-          <ul>
-            {this.state.items.map((item) => {
-              return(
-                <li key={item.id}>
-                <h3>{item.title}</h3>
-                <p>email address: {item.user}
-                {item.user === this.state.user.displayName || item.user === this.state.user.email ?
-                   <button onClick={() => this.removeItem(item.id)}>Remove Item</button> : null}
-                </p>
+  <section className='display-item'>
+  <div className="wrapper">
+    <ul>
+      {this.state.items.map((item) => {
+        return (
+          <li key={item.id}>
+            <h3>{item.title}</h3>
+            {/* TODO figure out how to  */}
+            <p>Subscriber First Name: {item.id}
+            {/* user: this.state.user.firstName || this.state.user.emailAddress */}
+            {item.user === this.state.user.firstName || item.emailAddress === this.state.user.emailAddress ?
+                   <button onClick={() => this.removeItem(item.id)} className="btn btn-primary">Remove Subscriber</button> : null}
+                   {/* <button><Link to='/SignUp' type="submit" className="btn btn-primary">Click to Sign Up for Special Offers</Link></button> */}
+            </p>
                 </li>
               )
             })}
           </ul>
           </div>
-          </section> */}
+          </section>
+    </div>
 
-        </div>
-        :
-        <div className='wrapper'>
-        <p>You must be logged in to see this page</p>
-        </div>  
-      }
+
+          </div>
+        ) : (
+          <div className="wrapper">
+            <p>You must be logged in to see this page</p>
+          </div>
+        )}
       </div>
-      // <div>
-      // <h2>Admin Log In</h2>
-      // Admin Login Form
-      //            <form>
-      //   <div class="form-group">
-      //     <label for="exampleInputEmail1">Email address</label>
-      //     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-      //     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      //   </div>
-      //   <div class="form-group">
-      //     <label for="exampleInputPassword1">Password</label>
-      //     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-      //   </div>
-      //    <div class="form-group form-check">
-      //     <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-      //     <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      //   </div> */}
-      //   <button type="submit" class="btn btn-primary">Submit</button>
-      // </form>
-
-      //             </div> */
     );
   }
+  
 }
 export default Admin;
