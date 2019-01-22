d53ff37ffe2edd2e882d3fc86a6759364a28ad5c
Shane Jacobson <shane-jacobson@hotmail.com>
2019-01-22 15:37:58 -0700

    Update to Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,19 +5,17 @@ class Home extends Component {
     render() {
         return (
             <text>
-                 {/* style={{textAlign: 'left'}}> */}
-<br/>
-<h1>To Fast or Not to Fast?  That is the question!!!!</h1>
 {/* Home page Paragraph 1 Text */}
 <div class="jumbotron jumbotron-fluid" style={{textAlign: 'left'}}>
   <div class="container">
+  <h1 align="center">To Fast or Not to Fast?  That is the question!!!!</h1>
+  <br/>
+  <br/>
     <h3 class="display-5">Nourishment, Movement, Rest, Hydration (Water and Minerals)</h3>
-    <br/>
     <p class="lead">Fasting is THE FASTest way to lose weight.  It’s THE FASTest way to help the body heal itself.  And, it’s THE FASTest way to obtaining ones health goals. Fasting entails the absence of food and sometimes even fluid.  There are so many myths surrounding fasting, that many people have shied away from it because of them.  The body has 2 methods of burning fuel for energy.  Our bodies either burn sugar or fat for fuel.  Ketosis is a state where the body is burning it’s own fat for fuel.  To get here, we need to exhaust the carbohydrate reserves in the liver.  As long as the body has fat to lose, you can fast while conserving muscle mass.  Fasting is the quickest way to get the body in to ketosis,  where the body can utilize fat for fuel.  Although fasting is extremely effectively, you need to make sure to support the body through a fast by providing all the necessary and essential minerals during it.  This will ensure a healthy and effective fast.  It will also allow you to fast for longer periods of time. 
 </p>
 <br/>
     <h3 class="display-5">Hidden Truths</h3>
-    <br/>
     <p class="lead">Follow me on this one.  All of us spent approximately 9 months bathing and floating in a pool full of salt water (amniotic fluid).  Our brains and our spinal chords swim in a sea of salt water called CSF (Cerebrospinal fluid).  This salty rich fluid circulates throughout our brains and our spinal chords.  Our tears and our sweat are salty as well.  It is one of the electrolytes that is essential to our heal and quality of life.  Sodium Chloride is not the same as sodium used to preserve many canned foods as well as that added to sugary energy drinks and packaged food.  When sodium has an imbalance of potassium to magnesium, the body can hang on to the sodium which in turn will retain fluid.
 <br/>
 <br/>
@@ -25,7 +23,6 @@ Contrary to popular belief, it is not calcium that makes our bones hard, but rat
 </p>
 <br/>
     <h3 class="display-5">Why Is Salt So Important?</h3>
-    <br/>
     <p class="lead">Salt is a compound that is made up of sodium and chlorine.  The human body is able to split this compound as needed, as both compounds are used in various functions of the human body.  Chlorine is used in many organ functions such as in the stomachs production of stomach acid.  Chlorine is used to make hydrochloric acid so we can digest food and absorb nutrients properly.  The body then uses sodium chloride as salt to keep the brain, spine, tears, bones, sweat glands, and organs maintaining their salt requirements.  
 <br/>
 <br/>
